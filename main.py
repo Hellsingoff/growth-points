38284f9cf817075914a93a8be5234c8500e78652
Hellsingoff <hellsingoff@gmail.com>
2021-03-15 13:09:35 +0500

    chat mute test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from os import getenv
+from datetime import datetime as dt
 
+from aiogram.types import ChatPermissions
 from dotenv import load_dotenv
 import logging
 from asyncio import sleep, CancelledError
@@ -45,6 +47,44 @@ async def send_message(user_id: int, text: str) -> bool:
     return False
 
 
+async def msg_switcher():
+    ban = ChatPermissions(can_send_messages=False,
+                          can_send_media_messages=False,
+                          can_send_polls=False,
+                          can_send_other_messages=False,
+                          can_add_web_page_previews=False,
+                          can_change_info=False,
+                          can_invite_users=True,
+                          can_pin_messages=False)
+    free = ChatPermissions(can_send_messages=True,
+                           can_send_media_messages=True,
+                           can_send_polls=False,
+                           can_send_other_messages=False,
+                           can_add_web_page_previews=True,
+                           can_change_info=False,
+                           can_invite_users=True,
+                           can_pin_messages=False)
+    while True:
+        await sleep(1)
+        # time = 7 >= dt.now().hour >= 19 or dt.now().weekday() == 6
+        time = dt.now().minute % 2
+        for chat_id in (-1001255431962, -1001255431962):  # поправить id
+            msg_perm = bot.get_chat(chat_id).permissions.can_send_messages
+            if msg_perm and time:
+                await bot.set_chat_permissions(chat_id, permissions=ban)
+                """await send_message(chat_id, "Уважаемые коллеги!\n" +
+                                            "По многочисленным просьбам мы ограничили возможность " +
+                                            "писать сообщения ночью и в воскресенье.\n" +
+                                            "пн 7:00 - 20:00\n" +
+                                            "вт 7:00 - 20:00\n" +
+                                            "ср 7:00 - 20:00\n" +
+                                            "чт 7:00 - 20:00\n" +
+                                            "пт 7:00 - 20:00\n" +
+                                            "сб 7:00 - 20:00")"""
+            elif not (msg_perm and time):
+                await bot.set_chat_permissions(chat_id, permissions=free)
+
+
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     # await send_message(message.from_user.id, 'Hi!')
@@ -60,4 +100,5 @@ async def error_log(*args):
 if __name__ == '__main__':
     log.info('Start.')
     load_dotenv()
+    dp.loop.create_task(msg_switcher())
     executor.start_polling(dp)
